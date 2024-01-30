9112abf3f0d96484b8f5f64ab7ff1a027b2796ab
Zoksss <zorantodorovic80@gmail.com>
2024-01-30 22:08:05 +0100

    fav icon and title update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import useLongPress from "./Component/useLongPress";
 
 
 import Timer from './Component/Timer/Timer';
@@ -13,21 +12,10 @@ function App() {
   const [isLogin, setIsLogin] = React.useState(true);
   const [isJoinedPlayers, setIsJoinedPlayers] = React.useState(false);
   const [isTimerScreen, setIsTimerScreen] = React.useState(false);
+  // eslint-disable-next-line
   const [username, setUsername] = React.useState("Zoksss");
 
-  const onLongPress = () => {
-    console.log('longpress is triggered');
-  };
 
-  const onClick = () => {
-    console.log('click is triggered')
-  }
-
-  const defaultOptions = {
-    shouldPreventDefault: true,
-    delay: 500,
-  };
-  const longPressEvent = useLongPress(onLongPress, onClick, defaultOptions);
 
   return (
     <div className="App">
